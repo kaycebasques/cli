57e63c31b40a342f7525bc2d67ad79188db9c017
Kayce Basques <kaycebasques@gmail.com>
2026-04-08 14:22:49 -0700

    Set up GPG keygen

--- .config/fish/conf.d/git.fish
+++ .config/fish/conf.d/git.fish
@@ -4,12 +4,16 @@ if not test -e $HOME/.ssh/id_ed25519.pub
     ssh-keygen -t ed25519 -C "kaycebasques@gmail.com"
     eval (ssh-agent -c)
     ssh-add $HOME/.ssh/id_ed25519
-    echo "TODO: Add the following key to https://github.com/settings/keys"
+    echo "TODO: Add the following key to https://github.com/settings/ssh/new"
     cat $HOME/.ssh/id_ed25519.pub
 end
 
 if not test -n "$(gpg --list-secret-keys --keyid-format=long 2>/dev/null)"
-    echo "No keys found. Generating now..."
+    gpg --full-generate-key
+    gpg --list-secret-keys --keyid-format=long
+    read -P "Enter the ID (the part after `sec ed25519/`):" key_id
+    gpg --armor --export $key_id
+    echo "TODO: Add the key to https://github.com/settings/gpg/new"
 end
 
 git config --global core.editor "vim"
