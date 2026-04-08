93b6883baa796b93a6c181155f88698fe3b49e2e
Kayce Basques <kaycebasques@gmail.com>
2026-04-08 15:47:32 -0700

    Fix GPG keygen over SSH again

--- gpg.fish
+++ gpg.fish
@@ -1,12 +1,23 @@
 #!/usr/bin/fish
 
-gpgconf --kill gpg-agent
+# Enable keygen over SSH
 # https://superuser.com/a/521027
-pinentry-program /usr/bin/pinentry-tty
+sudo apt install pinentry-tty
+set GPG_CONF $HOME/.gnupg/gpg-agent.conf
+if not test -f $GPG_CONF
+    touch $GPG_CONF
+end
+set PIN "pinentry-program /usr/bin/pinentry-tty"
+if not grep -Fxq "$PIN" "$GPG_CONF"
+    echo "$PIN" >> "$GPG_CONF"
+end
 gpg-connect-agent reloadagent /bye
+
+gpgconf --kill gpg-agent
 gpg --full-generate-key
 gpg --list-secret-keys --keyid-format=long
 read -P "Enter the **ed25519** **sec**ret ID:" key_id
 gpg --armor --export $key_id
 echo "TODO: Add the key to https://github.com/settings/gpg/new"
 git config --global user.signingkey $key_id
+
