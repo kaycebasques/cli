d3b884db4ea4e1856d324aedd4b97afa05ed12df
Kayce Basques <kaycebasques@gmail.com>
2026-07-06 14:46:39 -0700

    Init Vim customizations

---
+++ .vimrc
@@ -0,0 +1,4 @@
+syntax on
+set hlsearch
+filetype plugin indent on
+set list listchars=trail:·
--- update.fish
+++ update.fish
@@ -3,6 +3,7 @@
 set dir (status dirname)
 
 cp $dir/.bashrc $HOME/.bashrc
+cp $dir/.vimrc $HOME/.vimrc
 cp $dir/.config/user-dir.dirs $HOME/.config/user-dir.dirs
 cp $dir/.config/fish/conf.d/* $HOME/.config/fish/conf.d/
 cp $dir/.config/fish/functions/* $HOME/.config/fish/functions/
