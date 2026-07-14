4f45064c672785f96a92dcb67478baf64ff8393b
Kayce Basques <kaycebasques@gmail.com>
2026-07-14 09:22:57 -0700

    add $HOME/bin path

--- .config/fish/conf.d/paths.fish
+++ .config/fish/conf.d/paths.fish
@@ -1,3 +1,4 @@
-fish_add_path $HOME/.local/bin
-fish_add_path $HOME/.ghcup/bin
 fish_add_path $HOME/.cabal/bin
+fish_add_path $HOME/.ghcup/bin
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/bin
