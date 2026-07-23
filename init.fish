e0c228842cf6e054ed1609ecede058df3e528437
Kayce Basques <kaycebasques@gmail.com>
2026-07-23 10:20:28 -0700

    create symlinks during init

--- init.fish
+++ init.fish
@@ -1,7 +1,12 @@
 #!/usr/bin/env fish
 
 set dir (status dirname)
+set files .bashrc .vimrc .config/fish
 
-ln -s $dir/.bashrc $HOME/.bashrc
-ln -s $dir/.vimrc $HOME/.vimrc
-ln -s $dir/.config/fish $HOME/.config/fish
+for file in $files
+    set target $HOME/$file
+    if test -e $target; or test -L $target
+        rm -rf $target
+    end
+    ln -s $dir/$file $target
+end
