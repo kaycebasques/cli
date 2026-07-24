4c4437b872de991abd720c1b2d9da2065f3ef21d
Kayce Basques <kaycebasques@gmail.com>
2026-07-24 09:16:19 -0700

    only update after key press

--- .config/fish/conf.d/0.fish
+++ .config/fish/conf.d/0.fish
@@ -2,15 +2,20 @@
 
 set cli $HOME/cli
 
-if not command -s git > /dev/null
-  sudo apt install git
+echo "Press any key in next 3 seconds to update cli… "
+set -l key (bash -c 'read -t 3 -n 1 -s key; code=$?; echo "$key"; exit $code')
+set -l exit_code $status
+if test $exit_code -eq 0
+  echo "Updating…"
+  if not command -s git > /dev/null
+    sudo apt install git
+  end
+  git -C $cli fetch origin
+  git -C $cli rebase origin/main
 end
-git -C $cli fetch origin
-git -C $cli rebase origin/main
 
 cp $cli/.bashrc $HOME/.bashrc
 cp $cli/.vimrc $HOME/.vimrc
-
 test -d $HOME/.config/fish/conf.d; or mkdir -p $HOME/.config/fish/conf.d
 test -d $HOME/.config/fish/functions; or mkdir -p $HOME/.config/fish/functions
 cp $cli/.config/fish/conf.d/* $HOME/.config/fish/conf.d/
