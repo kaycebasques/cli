4d286efc4754dba94183fd03e44e642f05471834
Kayce Basques <kaycebasques@gmail.com>
2026-07-30 18:20:51 +0000

    source cargo before attempting download

--- .config/fish/conf.d/rust.fish
+++ .config/fish/conf.d/rust.fish
@@ -1,8 +1,8 @@
+if test -f "$HOME/.cargo/env.fish"
+  source "$HOME/.cargo/env.fish"
+end
+
 if not command -s cargo > /dev/null
     sudo apt install curl
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-end
-
-if test -f "$HOME/.cargo/env.fish"
-  source "$HOME/.cargo/env.fish"
 end
